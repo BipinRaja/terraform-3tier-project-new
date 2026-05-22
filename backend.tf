9832a4896f42d7be830765240d037a1d67dc6398
BipinRaja <86215042+BipinRaja@users.noreply.github.com>
2026-05-22 16:40:35 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -6,8 +6,8 @@ terraform {
     bucket         = "bipin-tfstate-bucket-123"
     key            = "3tier/prod/terraform.tfstate"
     region         = "ap-south-1"
-    # use_lockfile = true
-    dynamodb_table = "dynamodb-terraform-state-lock"
+    use_lockfile = true
+    # dynamodb_table = "dynamodb-terraform-state-lock"
     encrypt        = true
   }
 }
